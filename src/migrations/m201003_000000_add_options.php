dc109f142561d39a9cd2b62c2a08626e204ffca7
Josh Crawford <joshua@sgroup.com.au>
2020-10-06 10:09:34 +1100

    Fix `optionsSignature` error when updating

--- src/migrations/m201003_000000_add_options.php
+++ src/migrations/m201003_000000_add_options.php
@@ -5,6 +5,8 @@ use verbb\wishlist\elements\Item;
 
 use Craft;
 use craft\db\Migration;
+use craft\db\Query;
+use craft\helpers\Json;
 use craft\queue\jobs\ResaveElements;
 
 class m201003_000000_add_options extends Migration
@@ -16,7 +18,23 @@ class m201003_000000_add_options extends Migration
         }
 
         if (!$this->db->columnExists('{{%wishlist_items}}', 'optionsSignature')) {
-            $this->addColumn('{{%wishlist_items}}', 'optionsSignature', $this->string()->notNull()->after('options'));
+            $this->addColumn('{{%wishlist_items}}', 'optionsSignature', $this->string()->after('options'));
+
+            // Populate the values
+            $items = (new Query())
+                ->select(['*'])
+                ->from(['{{%wishlist_items}} items'])
+                ->all();
+
+            foreach ($items as $key => $item) {
+                $this->update('{{%wishlist_items}}', [
+                    'options' => Json::encode([]),
+                    'optionsSignature' => md5(Json::encode([]))
+                ], ['id' => $item['id']]);
+            }
+
+            // Mark the column as not null, now values have been imported
+            $this->alterColumn('{{%wishlist_items}}', 'optionsSignature', $this->string()->notNull());
         }
 
         return true;
