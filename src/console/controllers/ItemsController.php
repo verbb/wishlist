84de21bb94251ba071a68fa111cecd4a55e34a1d
Josh Crawford <joshua@sgroup.com.au>
2022-07-29 23:44:58 +1000

    Add `wishlist/items/cleanup-orphaned-items`

--- src/console/controllers/ItemsController.php
+++ src/console/controllers/ItemsController.php
@@ -2,10 +2,13 @@
 namespace verbb\wishlist\console\controllers;
 
 use verbb\wishlist\Wishlist;
+use verbb\wishlist\elements\Item;
 
 use Craft;
 use craft\console\Controller;
+use craft\db\Query;
 use craft\helpers\Console;
+use craft\helpers\Db;
 
 use yii\console\ExitCode;
 use yii\web\Response;
@@ -17,9 +20,24 @@ class ItemsController extends Controller
 
     public function actionCleanupOrphanedItems()
     {
-        $count = Wishlist::getInstance()->getLists()->purgeInactiveLists();
+        $itemElements = (new Query())
+            ->select(['id'])
+            ->from(['{{%elements}}'])
+            ->where(['type' => Item::class])
+            ->column();
 
-        $this->stderr('Purged ' . $count . ' lists.' . PHP_EOL, Console::FG_GREEN);
+        foreach ($itemElements as $itemElement) {
+            $item = (new Query())
+                ->from(['{{%wishlist_items}}'])
+                ->where(['id' => $itemElement])
+                ->exists();
+
+            if (!$item) {
+                $this->stderr('Removed item element ' . $itemElement . '.' . PHP_EOL, Console::FG_GREEN);
+
+                Db::delete('{{%elements}}', ['id' => $itemElement]);
+            }
+        }
 
         return ExitCode::OK;
     }
