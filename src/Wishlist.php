397188086e47f09aee371a1dd7867608a79e0df5
Josh Crawford <joshua@sgroup.com.au>
2023-12-29 20:48:58 +1100

    Element migrations

--- src/Wishlist.php
+++ src/Wishlist.php
@@ -52,7 +52,7 @@ class Wishlist extends Plugin
 
     public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
-    public string $schemaVersion = '1.0.5';
+    public string $schemaVersion = '1.1.0';
     public string $minVersionRequired = '1.4.11';
 
 
---
+++ src/migrations/m231229_000000_content_refactor.php
@@ -0,0 +1,43 @@
+<?php
+namespace verbb\wishlist\migrations;
+
+use verbb\wishlist\Wishlist;
+use verbb\wishlist\elements\Item;
+use verbb\wishlist\elements\ListElement;
+
+use craft\db\Query;
+use craft\migrations\BaseContentRefactorMigration;
+
+class m231229_000000_content_refactor extends BaseContentRefactorMigration
+{
+    // Public Methods
+    // =========================================================================
+
+    public function safeUp(): bool
+    {
+        foreach (Wishlist::$plugin->getListTypes()->getAllListTypes() as $type) {
+            $listIds = (new Query())->select('id')->from('{{%wishlist_lists}}')->where(['typeId' => $type->id])->scalar();
+
+            $this->updateElements(
+                $listIds,
+                $type->getFieldLayout(),
+            );
+
+            foreach ($listIds as $listId) {
+                $this->updateElements(
+                    (new Query())->from('{{%wishlist_items}}')->where(['listId' => $listId]),
+                    $type->getFieldLayout(),
+                );
+            }
+        }
+
+        return true;
+    }
+
+    public function safeDown(): bool
+    {
+        echo "m231229_000000_content_refactor cannot be reverted.\n";
+
+        return false;
+    }
+}
