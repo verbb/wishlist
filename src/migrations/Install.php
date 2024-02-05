5525f3e20379f9f51ec8ce068015c2f52436ebdc
Josh Crawford <joshua@sgroup.com.au>
2024-02-06 10:07:09 +1100

    Fix order of operations when uninstalling the plugin

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -29,9 +29,9 @@ class Install extends Migration
 
     public function safeDown(): bool
     {
+        $this->dropProjectConfig();
         $this->dropForeignKeys();
         $this->dropTables();
-        $this->dropProjectConfig();
 
         return true;
     }
