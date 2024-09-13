968f5bd8a9827eedd68008195db1a3ebe91ba323
Josh Crawford <joshua@sgroup.com.au>
2024-09-14 00:10:08 +1000

    Fix an inconsistency when purging empty lists, where items still exist, but no longer relate to an element

--- src/services/Lists.php
+++ src/services/Lists.php
@@ -408,7 +408,12 @@ class Lists extends Component
             ->offset($offset);
 
         if ($settings->purgeEmptyListsOnly) {
-            $query->andWhere(['is', '[[items.listId]]', null]);
+            // Check if there's no item records found, or if the item no longer links to an element
+            $query->andWhere([
+                'or',
+                ['is', '[[items.listId]]', null],
+                ['is', '[[items.elementId]]', null],
+            ]);
         }
 
         $userIds = $query->column();
@@ -428,7 +433,12 @@ class Lists extends Component
             ->offset($offset);
 
         if ($settings->purgeEmptyGuestListsOnly) {
-            $query->andWhere(['is', '[[items.listId]]', null]);
+            // Check if there's no item records found, or if the item no longer links to an element
+            $query->andWhere([
+                'or',
+                ['is', '[[items.listId]]', null],
+                ['is', '[[items.elementId]]', null],
+            ]);
         }
 
         $guestIds = $query->column();
